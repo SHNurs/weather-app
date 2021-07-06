3722affac5b5a6389ba0d46d0ccdbe3d2d442bf9
Shabdanov Nursultan Bekturganovich <shabdanov_n@auca.kg>
2021-07-06 22:43:16 +0600

    Add initial files

---
+++ public/serviceworker.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'version-1';
+const urls = [ 'index.html', 'offline.html'];
+
+const self = this;
+//Install SW
+self.addEventListener('install', (e)=> {
+    e.waitUntil(
+        caches.open(CACHE_NAME)
+        .then((cache) => {
+            return cache.addAll(urls);
+        })
+    );
+})
+//Listen for requests
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        caches.match(e.request)
+        .then(()=>{
+            return fetch(e.request)
+                .catch(() => caches.match('offline.html'))
+        })
+    )
+})
+
+//Activate the SW
+self.addEventListener('activate', (e) => {
+    const cacheWhiteList = [];
+    cacheWhiteList.push(CACHE_NAME);
+
+    e.waitUntil(
+        caches.keys().then(cacheNames => Promise.all(
+            cacheNames.map(cacheName => {
+                if(!cacheWhiteList.includes(cacheName)){
+                    return caches.delete(cacheName);
+                }
+            })
+        ))
+    )
+})
