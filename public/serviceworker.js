a92fa33db8003ab4dcd21b869f5195bdda007d8a
Shabdanov Nursultan Bekturganovich <shabdanov_n@auca.kg>
2021-07-06 23:03:28 +0600

    Fix manifest bugs

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,39 +1,44 @@
-const CACHE_NAME = 'version-1';
-const urls = [ 'index.html', 'offline.html'];
+const CACHE_NAME = "version-1";
+const urlsToCache = [ 'index.html', 'offline.html' ];
 
 const self = this;
-//Install SW
-self.addEventListener('install', (e)=> {
-    e.waitUntil(
+
+// Install SW
+self.addEventListener('install', (event) => {
+    event.waitUntil(
         caches.open(CACHE_NAME)
-        .then((cache) => {
-            return cache.addAll(urls);
-        })
-    );
-})
-//Listen for requests
-self.addEventListener('fetch', (e) => {
-    e.respondWith(
-        caches.match(e.request)
-        .then(()=>{
-            return fetch(e.request)
-                .catch(() => caches.match('offline.html'))
-        })
+            .then((cache) => {
+                console.log('Opened cache');
+
+                return cache.addAll(urlsToCache);
+            })
     )
-})
+});
 
-//Activate the SW
-self.addEventListener('activate', (e) => {
-    const cacheWhiteList = [];
-    cacheWhiteList.push(CACHE_NAME);
+// Listen for requests
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(() => {
+                return fetch(event.request) 
+                    .catch(() => caches.match('offline.html'))
+            })
+    )
+});
 
-    e.waitUntil(
-        caches.keys().then(cacheNames => Promise.all(
-            cacheNames.map(cacheName => {
-                if(!cacheWhiteList.includes(cacheName)){
+// Activate the SW
+self.addEventListener('activate', (event) => {
+    const cacheWhitelist = [];
+    cacheWhitelist.push(CACHE_NAME);
+
+    event.waitUntil(
+        caches.keys().then((cacheNames) => Promise.all(
+            cacheNames.map((cacheName) => {
+                if(!cacheWhitelist.includes(cacheName)) {
                     return caches.delete(cacheName);
                 }
             })
         ))
+            
     )
-})
+});
